26729850ad9545de6fd1991ba17ae3b177736393
jakeekajjakeekaj <yaricklozanomoctezuma1@gmail.com>
2023-11-29 19:49:32 -0600

    D 29-11-23 H 10-00-41

--- P17_PROPIEDADES_JS/code.js
+++ P17_PROPIEDADES_JS/code.js
@@ -18,3 +18,14 @@ h2_antiguo = document.querySelector(".h2"); //Como ya se ha visto previamente, a
     document.write("El elemento no tiene hijos");
 }*/
 
+console.log(h2_antiguo.parentElement);  //Indica el elemento padre
+console.log(h2_antiguo.parentNode); //Indica el nodo padre
+
+//------------------------- PROPIEDADES DE SIBLINGS (HERMANOS) ------------------------
+//LOS SIBLING O HERMANOS, SON LOS QUE ESTÁN A LA MISMA ALTURA, ES DECIR QUE NO SON NODOS PADRES O NODOS HIJOS, SINO QUE SE ENCUENTRAN A LA MISMA ALTURA DE JERARQUIZACIÓN
+
+console.log(h2_antiguo.previousElementSibling);    //Indica el hermano anterior
+
+console.log(h2_antiguo.nextElementSibling); //Indica el siguiente hermano
+
+console.log(h2_antiguo.closest(".contenedor")); //Busca al elemento padre más cercano
